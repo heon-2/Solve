892809dbdff873c4fd13922e0ff8954276a06b37
heon-2 <122588664+heon-2@users.noreply.github.com>
2024-02-12 10:14:47 +0900

    [level 2] Title: 올바른 괄호, Time: 7.63 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,27 +1,17 @@
-from collections import deque
 def solution(s):
-    q1 = deque(list(s))
-    q2 = deque()
-    first = q1.popleft()
-    
-    if first == ')' :
-        return False
-    else :
-        q2.append(first)
-        while q1 :
+    answer = True
+    q = []
+    s = list(s)
+    for i in s :
+        if i == "(" :
+            q.append("(")
+        elif i == ")" :
             try :
-                v = q1.popleft()
-                if v == ')' :
-                    q2.pop()
-                else :
-                    q2.append('(')
-            except:
-                return False
-    if q2 :
-        return False
-    else :
-        return True
-                
-            
-        
+                q.pop()
+            except :
+                answer = False
+                break
+    if q : 
+        answer = False
     
+    return answer
