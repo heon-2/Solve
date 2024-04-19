8cd0867a37d561c89862e0287ed80dddc76102a6
이지헌 <jiheon_114@naver.com>
2024-04-19 21:26:28 +0900

    solve: 프로그래머스 자바스크립트 문제 풀이

--- AlgorithmMemo(20240325~)/JavaScript/프로그래머스/Test.js
+++ AlgorithmMemo(20240325~)/JavaScript/프로그래머스/Test.js
@@ -1,5 +1,28 @@
-console.log(isNaN("text")); // true, "text"는 숫자로 변환될 수 없으므로
-console.log(Number.isNaN("text")); // false, "text"는 숫자 타입이 아니므로
+// console.log(isNaN("text")); // true, "text"는 숫자로 변환될 수 없으므로
+// console.log(Number.isNaN("text")); // false, "text"는 숫자 타입이 아니므로
 
-console.log(isNaN(NaN)); // true, NaN은 숫자가 아니므로
-console.log(Number.isNaN(NaN)); // true, NaN은 숫자 타입이고 값도 NaN이므로
+// console.log(isNaN(NaN)); // true, NaN은 숫자가 아니므로
+// console.log(Number.isNaN(NaN)); // true, NaN은 숫자 타입이고 값도 NaN이므로
+
+// const array1 = ["a", "b", "c"];
+// const array2 = ["d", "e", "f"];
+// const answer = array1.concat(array2);
+// console.log(answer);
+
+// const array1 = ["a", "b", "c", "d", "e"];
+// console.log(array1.copyWithin(0, 3, 4)); // [ 'd', 'b', 'c', 'd', 'e' ]
+
+// const isBelowThreshold = (cur) => cur < 40;
+// const array1 = [1, 30, 39, 29, 10, 13];
+// console.log(array1.every(isBelowThreshold)); // true
+
+// const array = [1, 2, 3, 4];
+// console.log(array.fill(0, 0, 2)); // [ 0, 0, 3, 4 ]
+
+// const words = ["a", "ab", "abc", "abcd", "abcde", "abcdef"];
+// const result = words.filter((word) => word.length > 2);
+// console.log(result); // [ 'abc', 'abcd', 'abcde', 'abcdef' ]
+
+const array = [1, 2, 3, 4, 5];
+const found = array.find((element) => element > 3);
+console.log(found);
